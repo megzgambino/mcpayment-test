91d046dd17d42ee384ad990c1602d4ba4e00bbea
Andhika Megantara <81455956+megzgambino@users.noreply.github.com>
2021-10-31 16:24:24 +0700

    fixes components in client, about to wiring to server

--- 2-budget-application/client/src/App.js
+++ 2-budget-application/client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <h1>BUDGET APP SIMPLIFIED</h1>
       <HomePage/>
     </div>
   )
--- 2-budget-application/client/src/pages/HomePage.js
+++ 2-budget-application/client/src/pages/HomePage.js
@@ -1,7 +1,51 @@
 import React from 'react'
 import './HomePage.css'
+import Swal from  'sweetalert2'
 
 export default function HomePage() {
+
+    function handleAddIncome () {
+        // console.log('masuk')
+        Swal.fire({
+            title: 'Add Income',
+            html: `<input type="income" id="income" class="swal2-input" placeholder="Income">`,
+            confirmButtonText: 'Add Income',
+            focusConfirm: false,
+            preConfirm: () => {
+              const income = Swal.getPopup().querySelector('#income').value
+              if (!income) {
+                Swal.showValidationMessage(`Please enter your desired income`)
+              }
+              return { income }
+            }
+          }).then((result) => {
+            Swal.fire(`
+              income: ${result.value.income}
+            `.trim())
+          })
+    }
+
+    function handleAddExpenses () {
+        Swal.fire({
+            title: 'Add Expenses',
+            html: `<input type="expenses" id="expenses" class="swal2-input" placeholder="Expenses">`,
+            confirmButtonText: 'Add Expenses',
+            focusConfirm: false,
+            preConfirm: () => {
+              const expenses = Swal.getPopup().querySelector('#expenses').value
+              if (!expenses) {
+                Swal.showValidationMessage(`Please enter your desired expenses`)
+              }
+              return { expenses }
+            }
+          }).then((result) => {
+            Swal.fire(`
+              expenses: ${result.value.expenses}
+            `.trim())
+          })
+    }
+
+
     return (
         <table id="users">
             <tr>
@@ -10,9 +54,18 @@ export default function HomePage() {
                 <th>Balance</th>
             </tr>
             <tr>
-                <td>500000</td>
-                <td>100000</td>
-                <td>1000000</td>
+                <td>
+                    500000
+                    <button id="button" type="submit" onClick={handleAddIncome}>Add Income</button>
+                </td>
+                <td>
+                    100000
+                    <button id="button" type="button" onClick={handleAddExpenses}>Add Expenses</button>
+                </td>
+                <td>
+                    1000000
+                    <button id="button">Add Balance</button>
+                </td>
             </tr>
         </table>
     )
